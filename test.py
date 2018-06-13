245220627e11b891449f2454ea0a7ec6a340a970
jmak <jmak@dexcom.com>
2018-06-12 22:10:56 -0700

    more changes?

--- test.py
+++ test.py
@@ -2,29 +2,27 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+
+driver = webdriver.Chrome('C:\\Users\\jmak2\\Desktop\\ChromeDriver.exe')
+driver.maximize_window()
 '''
 module for google login
 can't get password to work
 '''
+driver.get('http://google.com')
 
-# driver = webdriver.Chrome('C:\\Users\\jmak2\\Desktop\\ChromeDriver.exe')
-# driver.maximize_window()
-# driver.get('http://google.com')
+click_login = driver.find_element_by_id('gb_70')
+click_login.click()
 
-# click_login = driver.find_element_by_id('gb_70')
-# click_login.click()
+sign_in = driver.find_element_by_id('identifierId')
+sign_in.send_keys('jmak209@gmail.com')
+sign_in.send_keys(Keys.RETURN)
 
-# sign_in = driver.find_element_by_id('identifierId')
-# sign_in.send_keys('jmak209@gmail.com')
-# sign_in.send_keys(Keys.RETURN)
+driver.implicitly_wait(10)
 
-# driver.implicit_wait(10)
-
-# driver.find_element_by_xpath("//a[contains(.,'password')]")
-
-# enter_pass = driver.find_element_by_name('password')
-# enter_pass.send_keys('testestest')
-# enter_pass.send_keys(Keys.RETURN)
+enter_pass = driver.find_element_by_xpath("//div[containts(@class, 'Xb9hP')]")
+enter_pass.send_keys('testestest')
+enter_pass.send_keys(Keys.RETURN)
 
 
 
@@ -32,17 +30,15 @@ can't get password to work
 
 
 # module for netflix login
-driver = webdriver.Chrome('C:\\Users\\jmak2\\Desktop\\ChromeDriver.exe')
-driver.maximize_window()
-driver.get('http://www.netflix.com\login')
+# driver.get('http://www.netflix.com\login')
 
 
-sign_in = driver.find_element_by_id('lbl-email')
-sign_in.send_keys('jmak209@gmail.com')
+# sign_in = driver.find_element_by_id('lbl-email')
+# sign_in.send_keys('jmak209@gmail.com')
 
-enter_pass = driver.find_element_by_id('lbl-password')
-enter_pass.send_keys('')#enter password here
-enter_pass.send_keys(Keys.RETURN)
+# enter_pass = driver.find_element_by_id('lbl-password')
+# enter_pass.send_keys('')#enter password here
+# enter_pass.send_keys(Keys.RETURN)
 
 
 
